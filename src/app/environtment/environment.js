e2599c3c368107e4e4509a2dabe6de323f9eaef3
armeiitt <148439942+armeiitt@users.noreply.github.com>
2024-06-02 15:09:07 +0700

    up

--- src/app/environtment/environment.js
+++ src/app/environtment/environment.js
@@ -1,13 +1,3 @@
-// const environment = {
-// <<<<<<< HEAD
-//   API_DOMAIN: "10.30.232.103",
-// =======
-//   API_DOMAIN: "localhost",
-// >>>>>>> 85a26b73c9e65dde2ac0b227b77ebb236b648060
-//   API_PORT: "3000",
-// };
-
-// export default environment;
 const environment = {
   API_DOMAIN: "localhost",
   API_PORT: "3000",
