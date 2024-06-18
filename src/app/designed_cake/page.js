66fb7518f605f66077d85ff1ee5ba6d61f693f0f
armeiitt <148439942+armeiitt@users.noreply.github.com>
2024-06-18 14:58:45 +0700

    design_sex

--- src/app/designed_cake/page.js
+++ src/app/designed_cake/page.js
@@ -289,8 +289,8 @@ export default function Designed_Cake() {
 						<p className="mt-1 text-lg leading-8 text-gray-600"></p>
 					</div>
 					{/* <form action="#" method="POST" className="mx-auto mt-2 max-w-xl sm:mt-2"> */}
-					<div className="grid grid-cols-2 gap-y-6 sm:grid-cols-2 px-10">
-						<div className="enter_name_des">
+					<div className="pl-8">
+						{/* <div className="enter_name_des">
 							<label
 								htmlFor="message"
 								className="title_designed_cake_left block text-sm font-semibold leading-6 text-gray-900"
@@ -301,13 +301,34 @@ export default function Designed_Cake() {
 								name="message"
 								id="message"
 								rows={2}
-								className="mt-2.5 block w-full h-10 rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+								className="block w-full md:w-2/3 h-10 rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
 								value={cakeName}
 								onChange={handleCakeNameChange}
 							/>
-						</div>
 
-						<div className="grid grid-cols-1 sm:grid-cols-3 gap-4 mt-4">
+
+						</div> */}
+
+						<div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-4" style={{ justifyItems: 'space-between' }}>
+							<div className="sm:col-span-1 ">
+								<label
+									htmlFor="message"
+									className="title_designed_cake_left block text-sm font-semibold leading-6 text-gray-900 overflow-wrap break-word pb-4"
+									style={{ width: '100%' }}
+								>
+									Let's enter your name
+								</label>
+
+								<textarea
+									name="message"
+									id="message"
+									rows={2}
+									className="block w-full md:w-1/2 h-8 rounded-md border-0 px-3 py-2 text-sm text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
+									value={cakeName}
+									onChange={handleCakeNameChange}
+								/>
+
+							</div>
 							<div className="sm:col-span-1 ">
 								<label
 									htmlFor="shape"
@@ -358,7 +379,7 @@ export default function Designed_Cake() {
 								>
 									Size
 								</label>
-								<div className="mt-2">
+								<div className="mt-2 ">
 									<Dropdown>
 										<DropdownTrigger>
 											<Button variant="bordered" className="capitalize">
